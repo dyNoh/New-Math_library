c365d6d1485a3120abf42e784fa7a286747edbd0
Daeyeon <nararo1@naver.com>
2016-11-05 14:53:24 +0900

    make basicArith.h

---
+++ BasicArith/basicArith.h
@@ -0,0 +1,14 @@
+#ifndef __BASIC_ARITH_H__
+#define __BASIC_ARITH_H__
+
+/*
+    기본적인 연산에 관련된 헤더파일
+*/
+#define PI 3.1415926535
+
+double Add(double num1, double num2);   // num1 + num2
+double Min(double num1, double num2);   // num1 - num2
+double Mul(double num1, double num2);   // num1 * num2
+double Div(double num1, double num2);   // num1 / num2
+
+#endif
