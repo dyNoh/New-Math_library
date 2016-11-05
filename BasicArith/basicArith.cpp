f913689688b15dad574048486b48faa1dce020df
dyNoh <nararo1@naver.com>
2016-11-05 19:51:26 +0900

    make basicArith.cpp

---
+++ BasicArith/basicArith.cpp
@@ -0,0 +1,21 @@
+#include "basicArith.h"
+
+double Add(double num1, double num2)
+{
+	return 0.0;
+}
+
+double Min(double num1, double num2)
+{
+	return 0.0;
+}
+
+double Mul(double num1, double num2)
+{
+	return 0.0;
+}
+
+double Div(double num1, double num2)
+{
+	return 0.0;
+}
