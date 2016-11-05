a5345bb52c0b933d8b09742bb705a07a0da844a7
dyNoh <nararo1@naver.com>
2016-11-05 21:14:49 +0900

    update point.h

--- Figure/Point/point.h
+++ Figure/Point/point.h
@@ -15,13 +15,13 @@ public:
 	Point(const Point& p);
 	void setPoint(const double& x, const double& y);
 	void setPoint(const Point& p);
-	void setX(const double x);
-	void setY(const double y);
+	void setX(const double& x);
+	void setY(const double& y);
 	Point getPoint(void) const;
 	double getX(void) const;
 	double getY(void) const;
-	double distance(Point& p);			// 다른 점과의 거리
-	void showData(void) const;				// 정보 나타내기
+	double distance(const Point& p);	// 다른 점과의 거리
+	void showData(void) const;			// 정보 나타내기
 	~Point();							// 소멸자
 };
 #endif
