c1a87e35f49e29ca0367c601515332e843d682d2
dyNoh <nararo1@naver.com>
2016-11-05 19:54:02 +0900

    make point.cpp

---
+++ Figure/Point/point.cpp
@@ -0,0 +1,41 @@
+#include "point.h"
+
+Point::Point(double x, double y)
+{
+}
+
+void Point::setPoint(const double & x, const double & y)
+{
+}
+
+void Point::setPoint(const Point & p)
+{
+}
+
+void Point::setX(const double x)
+{
+}
+
+void Point::setY(const double y)
+{
+}
+
+Point Point::getPoint(void) const
+{
+	return Point();
+}
+
+double Point::getX(void) const
+{
+	return 0.0;
+}
+
+double Point::getY(void) const
+{
+	return 0.0;
+}
+
+double Point::distance(Point & p)
+{
+	return 0.0;
+}
