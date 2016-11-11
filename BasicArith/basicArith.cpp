996d563b19457e606a52d5e5d2852c0de422c482
Daeyeon Noh <nararo1@naver.com>
2016-11-11 16:01:19 +0900

    make double Sqrt(double num)

--- BasicArith/basicArith.cpp
+++ BasicArith/basicArith.cpp
@@ -19,3 +19,8 @@ double Div(double num1, double num2)
 {
 	return 0.0;
 }
+
+double Sqrt(double num)
+{
+	return 0.0;
+}
--- BasicArith/basicArith.h
+++ BasicArith/basicArith.h
@@ -10,5 +10,6 @@ double Add(double num1, double num2);   // num1 + num2
 double Min(double num1, double num2);   // num1 - num2
 double Mul(double num1, double num2);   // num1 * num2
 double Div(double num1, double num2);   // num1 / num2
+double Sqrt(double num);                // Square root, 제곱근
 
 #endif
