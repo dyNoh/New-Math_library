d938d4804cb1dffcee7111b1725fcfd03722afee
Daeyeon Noh <nararo1@naver.com>
2016-11-11 22:24:10 +0900

    make Triangle.cpp, Triangle.h

---
+++ Figure/Triangle/Triangle.cpp
@@ -0,0 +1,120 @@
+#include "Triangle.h"
+
+Triangle::Triangle(const double & t0_x, const double & t0_y, const double & t1_x, const double & t1_y, const double & t2_x, const double & t2_y)
+{
+}
+
+Triangle::Triangle(const Point & p1, const Point & p2, const Point & p3)
+{
+}
+
+Triangle::Triangle(const Point & p1, const Point & p2, const double & t2_x, const double & t2_y)
+{
+}
+
+Triangle::Triangle(const Point & p1, const double & t1_x, const double & t1_y, const double & t2_x, const double & t2_y)
+{
+}
+
+void Triangle::SetTriangle(const double & t0_x, const double & t0_y, const double & t1_x, const double & t1_y, const double & t2_x, const double & t2_y)
+{
+}
+
+void Triangle::SetTriangle(const Point & p1, const Point & p2, const Point & p3)
+{
+}
+
+void Triangle::SetTriangle(const Point & p1, const Point & p2, const double & t2_x, const double & t2_y)
+{
+}
+
+void Triangle::SetTriangle(const Point & p1, const double & t1_x, const double & t1_y, const double & t2_x, const double & t2_y)
+{
+}
+
+void Triangle::SetP0(const double & x, const double & y)
+{
+}
+
+void Triangle::SetP0(const Point & p)
+{
+}
+
+void Triangle::SetP1(const double & x, const double & y)
+{
+}
+
+void Triangle::SetP1(const Point & p)
+{
+}
+
+void Triangle::SetP2(const double & x, const double & y)
+{
+}
+
+void Triangle::SetP2(const Point & p)
+{
+}
+
+Triangle Triangle::GetTriangle(void) const
+{
+	return Triangle();
+}
+
+Point Triangle::GetP0(void) const
+{
+	return Point();
+}
+
+Point Triangle::GetP1(void) const
+{
+	return Point();
+}
+
+Point Triangle::GetP2(void) const
+{
+	return Point();
+}
+
+double Triangle::GetArea(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetRound(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetCenterOfGravity(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetOrthcenter(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetExcenter(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetIntercenter(void) const
+{
+	return 0.0;
+}
+
+double Triangle::GetCircumcenter(void) const
+{
+	return 0.0;
+}
+
+void Triangle::ShowData(void) const
+{
+}
+
+Triangle::~Triangle()
+{
+}
---
+++ Figure/Triangle/Triangle.h
@@ -0,0 +1,41 @@
+#ifndef __TRIANGLE_H
+#define __TRIANGLE_H
+
+#include "../Point/point.h"
+
+class Triangle
+{
+private:
+	Point t[3];
+public:
+	Triangle(const double& t0_x = 0, const double& t0_y = 0, const double& t1_x = 1, const double& t1_y = 0, const double& t2_x = 0, const double& t2_y = 1);
+	Triangle(const Point& p1, const Point& p2, const Point& p3);
+	Triangle(const Point& p1, const Point& p2, const double& t2_x, const double& t2_y);
+	Triangle(const Point& p1, const double& t1_x, const double& t1_y, const double& t2_x, const double& t2_y);
+	void SetTriangle(const double& t0_x, const double& t0_y, const double& t1_x, const double& t1_y, const double& t2_x, const double& t2_y);
+	void SetTriangle(const Point& p1, const Point& p2, const Point& p3);
+	void SetTriangle(const Point& p1, const Point& p2, const double& t2_x, const double& t2_y);
+	void SetTriangle(const Point& p1, const double& t1_x, const double& t1_y, const double& t2_x, const double& t2_y);
+	void SetP0(const double& x, const double& y);
+	void SetP0(const Point& p);
+	void SetP1(const double& x, const double& y);
+	void SetP1(const Point& p);
+	void SetP2(const double& x, const double& y);
+	void SetP2(const Point& p);
+	Triangle GetTriangle(void) const;
+	Point GetP0(void) const;
+	Point GetP1(void) const;
+	Point GetP2(void) const;
+	double GetArea(void) const;		// 넓이
+	double GetRound(void) const;	// 둘레
+	double GetCenterOfGravity(void) const;	// 무게중심
+	double GetOrthcenter(void) const;	// 수심
+	double GetExcenter(void) const;	// 방심
+	double GetIntercenter(void) const;	// 내심
+	double GetCircumcenter(void) const;	// 외심
+	void ShowData(void) const;	// 정보 나타내기
+	// 삼각형 각도 구하기 등 추가
+	~Triangle();	// 소멸자
+};
+
+#endif
