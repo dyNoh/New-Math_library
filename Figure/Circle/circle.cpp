72718021f037b362c362cb8e203ee44e6325149a
Hong-PC\Hong <wlgjs005@naver.com>
2016-11-16 17:51:02 +0900

    update circle.cpp Visual Studio에서 오류 확인 후 전체적으로 수정함.

--- Figure/Circle/circle.cpp
+++ Figure/Circle/circle.cpp
@@ -1,100 +1,94 @@
-	#include <iostream>
-	#include "circle.h"
+#include <iostream>
+#include "circle.h"
 
-	using std::cout;
-	using std::endl;
+using std::cout;
+using std::endl;
 
-	Circle::Circle(double x, double y, double r)
-	{
-		this->x = x;
-		this->y = y;
-		this->r = r;
+Circle::Circle(double x, double y, double r)
+{
+	this->p.SetPoint(x, y);
+	this->r = r;
 
-	}
+}
 
-	Circle::Circle(const Point & p, const double & r)
-	{
-		this->p->x = p.x;
-		this->p->y = p.y;
-		this->r = r;
+Circle::Circle(const Point & p, const double & r)
+{
+	this->p.SetPoint(p);
+	this->r = r;
 
-	}
+}
 
-	Circle::Circle(const Circle & c)
-	{
-		this->p->x = c.p->x;
-		this->p->y = c.p->y;
-	}
+Circle::Circle(const Circle & c)
+{
+	this->p.SetPoint(c.p);
+	this->r = c.r;
+}
 
-	void Circle::SetCircle(const double & x, const double & y, const double & r)
-	{
-		this->x = x;
-		this->y = y;
-		this->r = r;
-	}
+void Circle::SetCircle(const double & x, const double & y, const double & r)
+{
+	this->p.SetPoint(x, y);
+	this->r = r;
+}
 
-	void Circle::SetCircle(const Point & p, const double & r)
-	{
-		this->p->x = p.x;
-		this->p->y = p.y;
-		this->r = r;
-	}
+void Circle::SetCircle(const Point & p, const double & r)
+{
+	this->p.SetPoint(p);
+	this->r = r;
+}
 
-	void Circle::SetCircle(const Circle & c)
-	{
-		this->p->x = c.p->x;
-		this->p->y = c.p->y;
-	}
+void Circle::SetCircle(const Circle & c)
+{
+	this->p.SetPoint(c.p);
+	this->r = c.r;
+}
 
-	void Circle::SetCenter(const Point & p)
-	{
-		this->p->x = p.x;
-		this->p->y = p.y;
-	}
+void Circle::SetCenter(const Point & p)
+{
+	this->p.SetPoint(p);
+}
 
-	void Circle::SetCenter(const double & x, const double & y)
-	{
-		this->x=x;
-		this->y=y;
-	}
+void Circle::SetCenter(const double & x, const double & y)
+{
+	this->p.SetPoint(p);
+}
 
-	void Circle::SetR(const double & r)
-	{
-		this->r=r;
-	}
+void Circle::SetR(const double & r)
+{
+	this->r = r;
+}
 
-	Circle Circle::GetCircle(void) const
-	{
-		return Circle(this->p,this->p,this->r);
-	}
+Circle Circle::GetCircle(void) const
+{
+	return Circle(this->p, this->r);
+}
 
-	Point Circle::GetPoint(void) const
-	{
-		return Point(p->x,p->y);
-	}
+Point Circle::GetPoint(void) const
+{
+	return Point(this->p);
+}
 
-	double Circle::GetR(void) const
-	{
-		return this->r;
-	}
+double Circle::GetR(void) const
+{
+	return this->r;
+}
 
-	double Circle::GetArea(void) const
-	{
-		return ( 2 * PI * Pow(GetR()) );
-	}
+double Circle::GetArea(void) const
+{
+	return (2 * PI * GetR());
+}
 
-	double Circle::GetRound(void) const
-	{
-		return ( PI * Pow(GetR()) * Pow(GetR()) );
-	}
+double Circle::GetRound(void) const
+{
+	return (PI * Pow(GetR()));
+}
 
-	void Circle::ShowData(void)
-	{
-		cout << "x : " << GetPoint().GetX() << endl;
-		cout << "y : " << GetPoint().GetY() << endl;
-		cout << "r : " << GetR() << endl;
-	}
+void Circle::ShowData(void)
+{
+	cout << "x : " << GetPoint().GetX() << endl;
+	cout << "y : " << GetPoint().GetY() << endl;
+	cout << "r : " << GetR() << endl;
+}
 
-	Circle::~Circle()
-	{
-	}
+Circle::~Circle()
+{
+}
