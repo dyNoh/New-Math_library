4773200de6f457ff73f90db026dc62686ce4fcac
유병필\유병필 <comme6099@gmail.com>
2016-11-18 02:09:35 +0900

    update line.cpp
    void Line::SetLine(const Point & p1, const Point & p2)
    void Line::SetLine(const Point & p1, const double & l1_x, const double & l1_y)
    void Line::SetLine(const Line & l)
    void Line::SetP0(const double & x, const double & y)
    void Line::SetP0(const Point & p)
    void Line::SetP1(const double & x, const double & y)
    void Line::SetP1(const Point & p)
    Line Line::GetLine(void) const
    Point Line::GetP0(void) const
    Point Line::GetP1(void) const

--- Figure/Line/line.cpp
+++ Figure/Line/line.cpp
@@ -22,52 +22,63 @@ Line::Line(const Line & l)
 
 void Line::SetLine(const double & l0_x, const double & l0_y, const double & l1_x, const double & l1_y)
 {
-	this->t[0].SetLine(lo_x, l0_y);
-	this->t[1].SetLine(l1_x, l1_y);
+	this->l[0].SetLine(lo_x, l0_y);
+	this->l[1].SetLine(l1_x, l1_y);
 }
 
 void Line::SetLine(const Point & p1, const Point & p2)
 {
-
+	this->l[0].SetLine(p1.GetX(),p1.GetY());
+	this->l[1].SetLine(p2.GetX(),p2.GetY());
 }
 
 void Line::SetLine(const Point & p1, const double & l1_x, const double & l1_y)
 {
+	this->l[0].SetLine(p1.GetX(),p1.GetY());
+	this->l[1].SetLine(l1_x,l1_y);
 }
 
 void Line::SetLine(const Line & l)
 {
+	this->l[0].SetLine(l.GetP0());
+	this->l[1].SetLine(l.GetP1());
 }
 
 void Line::SetP0(const double & x, const double & y)
 {
+	this->l[0].SetLine(x);
+	this->l[0].SetLine(y);
 }
 
 void Line::SetP0(const Point & p)
 {
+	this->l[0].SetLine(p.GetX(),p.GetY());
 }
 
 void Line::SetP1(const double & x, const double & y)
 {
+	this->l[1].SetLine(x);
+	this->l[1].SetLine(y);
 }
 
 void Line::SetP1(const Point & p)
 {
+	this->l[0].SetLine(p.GetX(),p.GetY());
 }
 
 Line Line::GetLine(void) const
 {
-	return Line();
+	return Line(this->GetP0(),this->GetP1());
 }
 
 Point Line::GetP0(void) const
 {
-	return Point();
+	return Point(this->l[0].GetLine());
 }
 
 Point Line::GetP1(void) const
 {
-	return Point();
+	return Point(this->l[1].GetLine());
 }
 
 double Line::Distance(void) const
